0c30add028736fa5b37f8784fb04c916ccfc1074
Cedric Sarigumba <cediesarigumba@gmail.com>
2023-05-13 22:47:31 +0800

    Add implementation for sign-in

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone_tutorial/common/widgets/custom_button.dart';
 import 'package:amazon_clone_tutorial/common/widgets/custom_textfield.dart';
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:flutter/material.dart';
@@ -37,12 +38,16 @@ class _AuthScreenState extends State<AuthScreen> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text(
                 'Welcome',
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
               ListTile(
+                tileColor: _auth == Auth.signup
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundCOlor,
                 title: const Text('Create Account',
                     style: TextStyle(fontWeight: FontWeight.bold)),
                 leading: Radio(
@@ -77,11 +82,16 @@ class _AuthScreenState extends State<AuthScreen> {
                           controller: _passwordController,
                           hintText: "Password",
                         ),
+                        const SizedBox(height: 10),
+                        CustomButton(text: "Sign up", onTap: () {})
                       ],
                     ),
                   ),
                 ),
               ListTile(
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundCOlor,
                 title: const Text('Sign In',
                     style: TextStyle(fontWeight: FontWeight.bold)),
                 leading: Radio(
@@ -94,6 +104,29 @@ class _AuthScreenState extends State<AuthScreen> {
                       });
                     }),
               ),
+              if (_auth == Auth.signin)
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: "Email",
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: "Password",
+                        ),
+                        const SizedBox(height: 10),
+                        CustomButton(text: "Sign in", onTap: () {})
+                      ],
+                    ),
+                  ),
+                ),
             ],
           ),
         ),
