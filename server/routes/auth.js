807267f82ea40be65509e56c760c72969c0fcb4f
Cedric Sarigumba <cediesarigumba@gmail.com>
2023-05-13 23:43:47 +0800

    Add User model

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,8 @@ const express = require('express');
 const authRouter = express.Router();
 
 authRouter.post('/api/signup', (req, res) => {
+  const [name, email, password] = req.body;
+
   res.send('signup');
 });
 
