d94fff262df4b3baed8a22bd9bfddf468d3a8a51
Cedric Sarigumba <cediesarigumba@gmail.com>
2023-05-17 22:19:57 +0800

    feat: HOME: Added deal of the day.

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:amazon_clone_tutorial/constants/global_variables.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/address_box.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/carousel_image.dart';
+import 'package:amazon_clone_tutorial/features/home/widgets/deal_of_day.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 
@@ -87,14 +88,18 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: const [
-          AddressBox(),
-          SizedBox(height: 10),
-          TopCategories(),
-          SizedBox(height: 10),
-          CarouselImage(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            AddressBox(),
+            SizedBox(height: 10),
+            TopCategories(),
+            SizedBox(height: 10),
+            CarouselImage(),
+            SizedBox(height: 10),
+            DealOfDay(),
+          ],
+        ),
       ),
     );
   }
